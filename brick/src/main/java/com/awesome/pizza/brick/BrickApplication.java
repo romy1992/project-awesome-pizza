da9961c288559c328c5399075c8afbe3e73f1c35
Rosario Trotta <rosario.trotta@euris.it>
2025-09-24 18:58:11 +0200

    creazione moduli brick e commons

---
+++ brick/src/main/java/com/awesome/pizza/brick/BrickApplication.java
@@ -0,0 +1,12 @@
+package com.awesome.pizza.brick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrickApplication.class, args);
+    }
+}
+
